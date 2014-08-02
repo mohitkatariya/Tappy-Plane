93af9f18fba029381793f7fd2602dbdd3e5ab061
mohit <mohit@mohits-MacBook-Pro.local>
2014-08-02 14:17:52 +0530

    Initial Commit

---
+++ AirShip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AirShip
+//
+//  Created by mohit  on 02/08/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
